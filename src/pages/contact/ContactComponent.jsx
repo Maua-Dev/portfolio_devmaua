9ab43f608763ff7a643f3ca1607a8af434b7bc1e
Caio Toledo <caio.detoledo15@gmail.com>
2022-01-27 18:55:18 -0300

    ensinando a comitar

--- src/pages/contact/ContactComponent.jsx
+++ src/pages/contact/ContactComponent.jsx
@@ -4,7 +4,7 @@ import "./Contact.css";
 export default function ContactComponent() {
   return (
     <div className="ContactComponent pages" id="Contato">
-      <h1 className="titulo">Contato</h1>
+      <h1 className="titulo">Contato a</h1>
     </div>
   );
 }
