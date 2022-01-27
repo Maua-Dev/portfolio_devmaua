e7ecde56818259295dcde74f5f967877b688bf59
VgsStudio <vgsoller@gmail.com>
2022-01-27 20:56:38 -0300

    Primeiro commit branch member-page

--- src/pages/members/MembersComponent.jsx
+++ src/pages/members/MembersComponent.jsx
@@ -5,6 +5,8 @@ export default function MembersComponent() {
   return (
     <div className="MembersComponent pages" id="membros">
       <h1 className="titulo">Membros</h1>
+      <h1 className="titulo">First commit</h1>
+      
     </div>
   );
 }
