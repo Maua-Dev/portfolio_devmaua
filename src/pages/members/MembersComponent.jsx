5e1350145a9b8eff7aba6135649b677783c0b354
VgsStudio <vgsoller@gmail.com>
2022-01-28 15:56:12 -0300

    Tornando a Navbar Responsiva para mobile

--- src/pages/members/MembersComponent.jsx
+++ src/pages/members/MembersComponent.jsx
@@ -5,8 +5,6 @@ export default function MembersComponent() {
   return (
     <div className="MembersComponent pages" id="membros">
       <h1 className="titulo">Membros</h1>
-      <h1 className="titulo">First commit</h1>
-      
     </div>
   );
 }
