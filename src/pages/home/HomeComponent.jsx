61a2b716dd5218999dff4a7233e6e4f422f97ec9
VgsStudio <vgsoller@gmail.com>
2022-01-28 09:37:32 -0300

    Fundo da home com o parallax com o mouse do logo + responsividade da home

--- src/pages/home/HomeComponent.jsx
+++ src/pages/home/HomeComponent.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import "./Home.css";
+import logo from "./assets/LogoInteiro.svg";
+import {
+  MouseParallaxContainer,
+  MouseParallaxChild,
+} from "react-parallax-mouse";
 
 export default function HomeComponent() {
   return (
     <div className="HomeBackground" id="home">
-      <video
-        className="VideoBg"
-        autoPlay
-        loop
-        muted
-        src={"https://i.imgur.com/M7dR7Aj.mp4"}
-        type="video/mp4"
-      ></video>
+      <MouseParallaxContainer className="container" resetOnLeave>
+        <MouseParallaxChild factorX={0.03} factorY={0.05} className="logo-container">
+          <img
+            src={logo}
+            alt="Logo inteiro Dev. Community"
+            className="HomeLogo"
+          ></img>
+        </MouseParallaxChild>
+        <video
+          className="VideoBg"
+          autoPlay
+          loop
+          muted
+          src={"https://i.imgur.com/cDy9FHG.mp4"}
+          type="video/mp4"
+        ></video>
+      </MouseParallaxContainer>
     </div>
   );
 }
