5875b820cb84bae821a72f47a8bab0935b47107d
VgsStudio <vgsoller@gmail.com>
2022-02-02 18:22:17 -0300

    Menu Dropdown

--- src/pages/members/MembersComponent.jsx
+++ src/pages/members/MembersComponent.jsx
@@ -5,16 +5,18 @@ import members from "./assets/dataMembers.json";
 export default function MembersComponent() {
   const [areaEscolhida, setAreaEscolhida] = useState("Diretoria");
   const [isOpen, setOpen] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   function chooseArea(area) {
     //  Para aparecer poder selecionar
     setOpen(false);
+    setDropdownOpen(false);
 
     setTimeout(() => {
-      // Para ter dellay e uma animaçãozinha
-      setOpen(true);
       setAreaEscolhida(area);
-    }, 500);
+      setOpen(true);
+      // Para ter dellay e uma animaçãozinha
+    }, 300);
   }
 
   return (
@@ -34,32 +36,58 @@ export default function MembersComponent() {
             );
           })}
         </div>
+        <div className="dropdown">
+          <div
+            className="dropdown-btn"
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+          >
+            {areaEscolhida}
+          </div>
+          {dropdownOpen && (
+            <div className={`dropdown-content ${dropdownOpen && "open"}`}>
+              {Object.keys(members).map((area) => {
+                return (
+                  <div
+                    className={`dropdown-item ${
+                      areaEscolhida === area && "dropdown-open"
+                    }`} // Se estiver igual, ele vai considerar que está aberto e vai ficar branco
+                    key={area}
+                    onClick={() => chooseArea(area)}
+                  >
+                    {area}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
         <div className={`members-container ${isOpen && "open"}`}>
           {members[areaEscolhida].map((member) => {
             // Quando ele está aberto (isOpen) ele vai rapidamente desligar a opacidade e mudá-la depois, para dar o efeito de smooth
 
-            function getMemberInformations() {
+            function getMemberInformations(member) {
               // Pega as informações do github do membro
-              let githubName = member.githubLink.split(".com/").pop();
-              let githubApiUrl = "https://api.github.com/users/" + githubName;
+              // let githubName = member.githubLink.split(".com/").pop();
+              // let githubApiUrl = "https://api.github.com/users/" + githubName;
 
               let memberInfo = {
-                name: "",
-                avatar: "",
-                bio: "",
+                name: "Vitor",
+                avatar: "https://avatars.githubusercontent.com/u/81604963?v=4",
+                bio: "19 anos.\r\nEstudante de Engenharia da Computação no Instituto Mauá de Tecnologia. \r\nCurioso, dedicado, proativo e extrovertido nas horas vagas",
               };
 
-              fetch(githubApiUrl)
-                .then((response) => response.json()) // Get from url and transform to a Json
-                .then((data) => {
-                  memberInfo.name = data.name;
-                  memberInfo.avatar = data.avatar;
-                  memberInfo.bio = data.bio;
-                });
+              // fetch(githubApiUrl)
+              //   .then((response) => response.json()) // Get from url and transform to a Json
+              //   .then((data) => {
+              //     memberInfo.name = data.name;
+              //     memberInfo.avatar = data.avatar;
+              //     memberInfo.bio = data.bio;
+              //   });
 
               return (
                 <div className="members" key={member.name}>
                   <img src={memberInfo.avatar} alt="" className="foto" />
+                  <h2 className="memberName">{member.name}</h2>
                 </div>
               );
             }
