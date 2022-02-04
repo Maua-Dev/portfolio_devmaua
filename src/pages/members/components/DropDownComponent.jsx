424c0b3a0aaf20652895f5fe5747311418043910
VgsStudio <vgsoller@gmail.com>
2022-02-03 22:37:39 -0300

    Detalhes no Dropdown menu

--- src/pages/members/components/DropDownComponent.jsx
+++ src/pages/members/components/DropDownComponent.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./DropDownComponent.css";
+import { AiFillCaretDown } from "react-icons/ai";
 
 export default function DropDownComponent(props) {
+  let firstArea = Object.keys(props.members)[0]; // Pega a PRIMEIRA área
+
   return (
     <div className="dropdown">
       <div
         className="dropdown-btn"
         onClick={() => props.setDropdownOpen(!props.dropdownOpen)}
       >
-        {props.areaEscolhida}
+        {props.areaEscolhida} <AiFillCaretDown className="setinha" />
       </div>
       {props.dropdownOpen && (
         <div className={`dropdown-content ${props.dropdownOpen && "open"}`}>
@@ -17,7 +20,8 @@ export default function DropDownComponent(props) {
               <div
                 className={`dropdown-item ${
                   props.areaEscolhida === area && "dropdown-open"
-                }`} // Se estiver igual, ele vai considerar que está aberto e vai ficar branco
+                } ${area === firstArea && "dropdown-first"} `}
+                // Se estiver igual, ele vai considerar que está aberto e vai ficar branco
                 key={area}
                 onClick={() => props.chooseArea(area)}
               >
