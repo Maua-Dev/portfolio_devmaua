191212dbd70bf074cbf1044d5f2308c87acc6266
VgsStudio <vgsoller@gmail.com>
2022-02-07 19:52:56 -0300

    Footer

--- src/pages/footer/FooterComponent.jsx
+++ src/pages/footer/FooterComponent.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Footer.css";
 
 export default function FooterComponent() {
-  return <div className="FooterComponent">EQUIPE DO ZEEBA NAQUELE NAIPÃO</div>;
+  return (
+    <div className="FooterComponent">
+      <p> © Dev. Community Mauá 2022</p>
+    </div>
+  );
 }
