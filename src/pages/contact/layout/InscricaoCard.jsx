ebd6e0f08c4ab3b4e37893b71ff5827eb0a3a429
Caio Kurek Marques Manuel <caiokurek@hotmail.com>
2022-02-07 21:24:56 -0300

    Responsividade Contact Page

---
+++ src/pages/contact/layout/InscricaoCard.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './InscricaoCard.css'
+import QR_Code from '../assets/QR_Code.png'
+
+export default function InscricaoCard() {
+
+    return (
+        <div className="Caixote">
+            <div className="Inscricao">
+                Nosso processo seletivo está aberto!
+                <ul className='List'>
+                    <li>Não precisa saber programar</li>
+                    <li>Vagas para todas as áreas</li>
+                </ul>
+            </div>
+            <div className="Link">
+                <a className='Botao' href="https://forms.gle/NG2FLfEjAW8V5r256" target="_blank">INSCREVA-SE</a>
+                <img src={QR_Code} alt="QR_Code formulário" className="QR_Code" />
+                <p>DE 07/02 ATÉ 07/03</p>
+            </div>
+        </div>
+    )
+}
