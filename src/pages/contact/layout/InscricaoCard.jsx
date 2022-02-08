31e985cd1ce02732375bded692b1cddf9299e6dd
Caio Kurek Marques Manuel <caiokurek@hotmail.com>
2022-02-07 21:28:44 -0300

    Responsividade Contact Page

--- src/pages/contact/layout/InscricaoCard.jsx
+++ src/pages/contact/layout/InscricaoCard.jsx
@@ -14,7 +14,7 @@ export default function InscricaoCard() {
                 </ul>
             </div>
             <div className="Link">
-                <a className='Botao' href="https://forms.gle/NG2FLfEjAW8V5r256" target="_blank">INSCREVA-SE</a>
+                <a className='Botao' href="https://forms.gle/NG2FLfEjAW8V5r256" target="_blank" rel="noreferrer">INSCREVA-SE</a>
                 <img src={QR_Code} alt="QR_Code formulário" className="QR_Code" />
                 <p>DE 07/02 ATÉ 07/03</p>
             </div>
