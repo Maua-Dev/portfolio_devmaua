20002cd8ba7d90388de02357896e65016ccb3fa2
Vitor Soller <vgsoller@gmail.com>
2022-02-13 11:44:12 -0300

    Modal Pop up primeira vez entrando no site

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import ProjectsComponent from "./pages/projects/ProjectsComponent";
 import MembersComponent from "./pages/members/MembersComponent";
 import ContactComponent from "./pages/contact/ContactComponent";
 import FooterComponent from "./pages/footer/FooterComponent";
+import ModalOnLoad from "./components/modal/ModalOnLoad";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <ModalOnLoad />
       <HomeComponent />
       <AboutUsComponent />
       <ProjectsComponent />
