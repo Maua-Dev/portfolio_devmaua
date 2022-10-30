41c21c51618a0248d2d9aa981c3598da9d8e967d
Enzo Sakamoto <enzo.sak@hotmail.com>
2022-10-30 13:18:38 -0300

    Ajuste nos cards do processo seletivo

--- src/pages/contact/layout/InscricaoCard.jsx
+++ src/pages/contact/layout/InscricaoCard.jsx
@@ -7,16 +7,16 @@ export default function InscricaoCard() {
     return (
         <div className="Caixote">
             <div className="Inscricao">
-                Nosso processo seletivo está aberto!
+                Nosso processo seletivo será aberto ano que vem!
                 <ul className='List'>
                     <li>Não precisa saber programar</li>
                     <li>Vagas para todas as áreas</li>
                 </ul>
             </div>
             <div className="Link">
-                <a className='Botao' href="https://forms.gle/NG2FLfEjAW8V5r256" target="_blank" rel="noreferrer">INSCREVA-SE</a>
+                {/* <a className='Botao' href="https://forms.gle/NG2FLfEjAW8V5r256" target="_blank" rel="noreferrer">INSCREVA-SE</a> */}
                 <img src={QR_Code} alt="QR_Code formulário" className="QR_Code" />
-                <p>DE 07/02 ATÉ 07/03</p>
+                <p>AGUARDE!</p>
             </div>
         </div>
     )
