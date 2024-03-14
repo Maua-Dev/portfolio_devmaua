ad8473ce845306068b78b1864233edb0d671e425
Felipe Carillo <felipecarillo@outlook.com>
2024-03-13 22:04:16 -0300

    Finished IAC configuration

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -4,18 +4,14 @@ import * as cdk from 'aws-cdk-lib';
 import { IacStack } from '../lib/iac-stack';
 
 const app = new cdk.App();
-new IacStack(app, 'IacStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const env = {
+  account: process.env.AWS_ACCOUNT_ID,
+  region: process.env.AWS_REGION
+}
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const stackName = process.env.STACK_NAME || 'PortfolioDevMauaStackDev'
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new IacStack(app, stackName, {
+  env
 });
--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,16 +1,133 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 
+import { Construct } from 'constructs';
 export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const stage = process.env.GITHUB_REF_NAME || 'dev'
+    const acmCertificateArn = process.env.ACM_CERTIFICATE_ARN || 'arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012'
+    const alternativeDomain = process.env.ALTERNATIVE_DOMAIN || "portfolio-dev.devmaua.com"
+    const hostedZoneIdValue = process.env.HOSTED_ZONE_ID || 'Z1UJRXOUMOOFQ8'
+    const projectName = process.env.PROJECT_NAME || 'PortfolioDevMauaFront'
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'IacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3Bucket = new s3.Bucket(this, 'PortfolioDevMauaFrontBucket' + stage, {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      autoDeleteObjects: true,
+    });
+
+    const oac = new cloudfront.CfnOriginAccessControl(this, "AOC", {
+      originAccessControlConfig: {
+        name: 'Portfolio Dev Maua Front Bucket OAC ' + stage,
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4'
+      }
+    })
+
+    let viewerCertificate = cloudfront.ViewerCertificate.fromCloudFrontDefaultCertificate();
+    if (stage === 'prod' || stage === 'homolog' || stage === 'dev') {
+      viewerCertificate = cloudfront.ViewerCertificate.fromAcmCertificate(
+        Certificate.fromCertificateArn(this, 'PortfolioDevMauaFrontCertificate-' + stage, acmCertificateArn),
+        {
+          aliases: [alternativeDomain],
+          securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+        },
+      )
+    }
+
+    const cloudFrontWebDistribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      'CDN',
+      {
+        comment: projectName + ' ' + stage,
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: s3Bucket,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+                allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD,
+                compress: true,
+                cachedMethods:
+                  cloudfront.CloudFrontAllowedCachedMethods.GET_HEAD,
+                viewerProtocolPolicy:
+                  cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                minTtl: cdk.Duration.seconds(0),
+                maxTtl: cdk.Duration.seconds(86400),
+                defaultTtl: cdk.Duration.seconds(3600)
+              }
+            ]
+          }
+        ],
+        viewerCertificate: viewerCertificate,
+        errorConfigurations: [
+          {
+            errorCode: 403,
+            responseCode: 200,
+            responsePagePath: '/index.html',
+            errorCachingMinTtl: 0
+          }
+        ]
+      }
+    );
+
+    const cfnDistribution = cloudFrontWebDistribution.node.defaultChild as cloudfront.CfnDistribution;
+
+    cfnDistribution.addPropertyOverride(
+      'DistributionConfig.Origins.0.OriginAccessControlId',
+      oac.getAtt('Id')
+    )
+
+    s3Bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['s3:GetObject'],
+        principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+        resources: [s3Bucket.arnForObjects('*')]
+      })
+    );
+
+    if (stage === 'prod' || stage === 'homolog' || stage === 'dev'){
+      const zone = route53.HostedZone.fromHostedZoneAttributes(
+        this,
+        'PortfolioDevMauaFrontHostedZone-' + stage,
+        {
+          hostedZoneId: hostedZoneIdValue,
+          zoneName: alternativeDomain
+        }
+      )
+
+        new route53.ARecord(this, 'PortfolioDevMauaFrontAliasRecord-' + stage, {
+          zone: zone,
+          recordName: alternativeDomain,
+          target: route53.RecordTarget.fromAlias(
+            new route53Targets.CloudFrontTarget(cloudFrontWebDistribution)
+          )
+        })
+      }
+
+      new cdk.CfnOutput(this, 'PortfolioDevMauaFrontBucketName-' + stage, {
+        value: s3Bucket.bucketName
+      })
+
+      new cdk.CfnOutput(this, 'PortfolioDevMauaFrontDistributionId-' + stage, {
+        value: cloudFrontWebDistribution.distributionId
+      })
+
+      new cdk.CfnOutput(this, 'PortfolioDevMauaFrontDistributionDomainName-' + stage, {
+        value: cloudFrontWebDistribution.distributionDomainName,
+      });
   }
 }
