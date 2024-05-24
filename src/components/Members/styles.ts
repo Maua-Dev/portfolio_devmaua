935f0599f23a6a50b37629a2eae8107878fa7122
Tiago Tokugi de Albuquerque Massuda <tiagomassuda123@gmail.com>
2024-05-24 09:56:09 -0300

    Ajuste no cardselect

--- src/components/Members/styles.ts
+++ src/components/Members/styles.ts
@@ -56,7 +56,7 @@ export const CardSelect = styled.div`
     padding: 3px;
     margin: 6px;
     border-radius: 8px;
-    border: 2px solid ${props => props.theme.colors.secondary};
+    border: 2px solid ${props => props.theme.title === 'light' ? props.theme.colors.blue.quaternary : props.theme.colors.red.primary};
   }
 
   @media (max-width: 768px) {
@@ -64,7 +64,7 @@ export const CardSelect = styled.div`
     padding: 2px;
     margin: 4px;
     border-radius: 5px;
-    border: 1px solid ${props => props.theme.colors.secondary};
+    border: 1px solid ${props => props.theme.title === 'light' ? props.theme.colors.blue.quaternary : props.theme.colors.red.primary};
   }
 
   &:hover {
