5e7207c1c7317723d75f52809587890ba3b88adf
tokujiTO <tiagomassuda123@gmail.com>
2025-11-03 21:20:54 -0300

    fix: iac config

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
-import { IacStack } from '../lib/iac-stack'
+import { IacStack } from '../lib/iac_stack'
 
 const app = new cdk.App()
 
@@ -12,6 +12,6 @@ const env = {
 
 const stackName = process.env.STACK_NAME || 'PortfolioStackDev'
 
-new IacStack(app, stackName, {
+new IacStack (app, stackName, {
   env: env
 })
